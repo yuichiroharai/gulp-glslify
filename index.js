eee4f949e28c696a4a2bf625485b583c9a239d01
Yuichiroh Arai <yuichiroh.arai@gmail.com>
2016-06-08 16:51:02 +0900

    add source code

---
+++ index.js
@@ -0,0 +1,60 @@
+// modules for gulp
+var through2 = require('through2');
+var gulpUtil = require('gulp-util');
+var PluginError = gulpUtil.PluginError;
+
+// modules for glslify
+var glslify = require('glslify');
+var glslifyImport = require('glslify-import');
+var glslifyHex = require('glslify-hex');
+
+var PLUGIN_NAME = 'gulp-glslify';
+
+function gulpGlslify(options) {
+
+    if (!options) options = {};
+
+    // inline mode
+    options.inline = true;
+
+    if (!options.transform) options.transform = [];
+
+    // use glslify-import & glslify-hex as default
+    options.transform.unshift(glslifyImport, glslifyHex);
+
+    var stream = through2.obj(function (file, enc, cb) {
+
+        // adjust to target file
+        options.basedir = file.history[0].split("/").slice(0, -1).join("/");
+
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            return;
+        }
+
+        var _this = this;
+        if (file.isBuffer()) {
+
+            glslify.bundle(file.contents.toString(), options, function(error, glsl, files) {
+                if (error) {
+                    cb(new PluginError(PLUGIN_NAME, error));
+                    return;
+                } else {
+                    file.contents = new Buffer(glsl);
+                    _this.push(file);
+                    return cb();
+                }
+            });
+        }
+
+    });
+
+    return stream;
+}
+
+module.exports = gulpGlslify;
